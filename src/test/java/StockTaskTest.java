0e8a4553eab542c5e991c7c066c0f022b22a07aa
MuchContact <liuhengjiangis@foxmail.com>
2016-10-31 19:38:12 +0800

    undo using HtmlUnitDriver

--- src/test/java/StockTaskTest.java
+++ src/test/java/StockTaskTest.java
@@ -25,7 +25,13 @@ public class StockTaskTest {
 
     @Before
     public void setUp() throws Exception {
-        driver = new HtmlUnitDriver();
+        String Xport = System.getProperty(
+                "lmportal.xvfb.id", ":1");
+        final File firefoxPath = new File(System.getProperty(
+                "lmportal.deploy.firefox.path", "/usr/bin/firefox"));
+        FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
+        firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
+        driver = new FirefoxDriver(firefoxBinary, null);
     }
 
     @Test
